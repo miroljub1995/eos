ab0821d3ceeb905349524378164be660e86148b1
miroljubtomic <miroljub1995@gmail.com>
2019-11-30 18:32:24 +0100

    added clock

--- src/default_apps/taskbar/src/App.js
+++ src/default_apps/taskbar/src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './App.scss';
 
 function App() {
 
@@ -15,30 +15,60 @@ function App() {
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name);
 
-    const appsViews = apps.map(app => {
-      const appFolder = join(appsFolder, app);
-      var appConfig = JSON.parse(fs.readFileSync(join(appFolder, 'app.json'), 'utf8'));
+  const appsViews = apps.map(app => {
+    const appFolder = join(appsFolder, app);
+    var appConfig = JSON.parse(fs.readFileSync(join(appFolder, 'app.json'), 'utf8'));
 
-      let start = appConfig.start;
-      if (appConfig.isFile) {
-        start = join("file://", appFolder, start);
-      }
-      
-      const imagePath = join(appFolder, appConfig.icon);
-      const appView = (
-        <div className="item" key={app} onClick={() => ipcRenderer.send('itemClick', { app, start })}>
-          <div className="icon" style={{backgroundImage: `url('${imagePath/*windows only*/.replace(/\\/g, '\\\\')}')`}}>
-          </div>
+    let start = appConfig.start;
+    if (appConfig.isFile) {
+      start = join("file://", appFolder, start);
+    }
+
+    const imagePath = join(appFolder, appConfig.icon);
+    const appView = (
+      <div className="app-item" key={app} onClick={() => ipcRenderer.send('itemClick', { app, start })}>
+        <div className="icon" style={{ backgroundImage: `url('${imagePath/*windows only*/.replace(/\\/g, '\\\\')}')` }}>
         </div>
-      );
-      return appView;
+      </div>
+    );
+    return appView;
   });
 
   return (
-    <div id="container" className="container">
-      {appsViews}
+    <div className="container">
+      <div className="apps">
+        {appsViews}
+      </div>
+      <div className="tray">
+        <Clock />
+      </div>
     </div>
   );
+}
+
+class Clock extends Component {
+
+  state = {
+    date: new Date(),
+    updateInterval: null
+  };
+
+  constructor() {
+    super();
+
+    this.state.updateInterval = setInterval(() => {
+      const date = new Date();
+      this.setState({ date })
+    }, 1000);
+  }
+
+  render() {
+    return (
+      <div className="tray-item">
+        {this.state.date.toLocaleTimeString()}
+      </div>
+    );
+  }
 }
 
 export default App;
